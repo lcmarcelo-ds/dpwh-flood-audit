3a3fd05306b44061d1f409c4d0af796f1b63a28e
lcmarcelo-ds <lcmarcelo@up.edu.ph>
2025-09-29 03:13:56 +0800

    Update textutils.py

--- dpwhlib/textutils.py
+++ dpwhlib/textutils.py
@@ -4,11 +4,11 @@ from difflib import SequenceMatcher
 STOPWORDS = {"of","the","and","for","to","in","a","an"}
 
 def norm_text(s: str) -> str:
-    if s is None: return ""
-    s = str(s).lower().strip()
+    s = "" if s is None else str(s).lower().strip()
     s = re.sub(r"[^a-z0-9\s\-/,_()&.]", " ", s)
     s = re.sub(r"\s+", " ", s).strip()
     return s
 
 def seq_ratio(a: str, b: str) -> float:
     return SequenceMatcher(None, norm_text(a), norm_text(b)).ratio()
+
